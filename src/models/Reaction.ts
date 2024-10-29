b9189ae774c7149e845ea64795173ddcdcba239a
theon87 <theonfrancis17@gmail.com>
2024-10-28 20:46:57 -0400

    create reactionSchema

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -6,3 +6,33 @@ interface IReaction extends Document {
     username: string;
     createdAt: Date;
 }
+
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),        
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+export default reactionSchema;
