30b4561c187cc66d091e9f235c6ada4d22a411c3
theon87 <theonfrancis17@gmail.com>
2024-10-28 20:52:33 -0400

    create getAllUsers

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,2 +1,11 @@
 import { User, Thought } from '../models/index.js';
 import { Request, Response } from 'express';
+
+export const getAllUsers = async(_req: Request, res: Response) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    }
