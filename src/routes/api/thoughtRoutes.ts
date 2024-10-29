e44d55305cc07bf1ffb668430142efcfd35236e4
theon87 <theonfrancis17@gmail.com>
2024-10-28 20:35:48 -0400

    add api thought routes in api folder

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+const router = Router();
+
+// WHEN I open API GET routes in Insomnia for users and thoughts
+// THEN the data for each of these routes is displayed in a formatted JSON
+
+import {
+  getAllThoughts,
+  createThought,
+  getThoughtById,  
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} from '../../controllers/thoughtController.js';
+
+// /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+export { router as thoughtsRouter };
