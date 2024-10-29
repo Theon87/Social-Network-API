8121bf227a1b0dc6fdfe30dee4736ba2ab398731
theon87 <theonfrancis17@gmail.com>
2024-10-28 20:34:00 -0400

    add api user routes to api folder

---
+++ src/routes/api/userRoutes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+const router = Router();
+
+// WHEN I open API GET routes in Insomnia for users and thoughts
+// THEN the data for each of these routes is displayed in a formatted JSON
+
+import {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} from '../../controllers/userController.js';
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+export { router as userRouter} ;
