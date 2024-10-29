cc9f64694ac1c417874387c73b4fd6d558e4a8b3
theon87 <theonfrancis17@gmail.com>
2024-10-28 21:01:30 -0400

    create getAllThoughts

--- src/controllers/thoughtControllers.ts
+++ src/controllers/thoughtControllers.ts
@@ -1,2 +1,12 @@
 import { Thought, User } from '../models/index.js';
 import { Request, Response } from 'express';
+
+// get all thoughts
+export const getAllThoughts = async(_req: Request, res: Response) => {
+    try {
+        const thoughts = await Thought.find();
+        res.json(thoughts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    }
