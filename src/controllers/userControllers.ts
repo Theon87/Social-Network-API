22899d7ba170ea23e7c472f95d0102355d17c445
theon87 <theonfrancis17@gmail.com>
2024-10-28 20:53:30 -0400

    create getUserById

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,6 +1,7 @@
 import { User, Thought } from '../models/index.js';
 import { Request, Response } from 'express';
 
+// get all users
 export const getAllUsers = async(_req: Request, res: Response) => {
     try {
         const users = await User.find();
@@ -9,3 +10,22 @@ export const getAllUsers = async(_req: Request, res: Response) => {
         res.status(500).json(err);
     }
     }
+
+// get a user by id
+export const getUserById = async (req: Request, res: Response) => {
+    const { userId } = req.params;
+    try {
+        const user = await User.findById(userId).select('-__v').populate('friends').populate('thoughts');
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({
+                message: 'User not found'
+            });
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
